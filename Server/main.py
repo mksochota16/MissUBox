2f4fe7ef2119e2a44026ace75fb99da4681f9c0a
Paweł Gryka <pawel.m.gryka@gmail.com>
2023-10-11 21:57:18 +0200

    Add finished server

--- Server/main.py
+++ Server/main.py
@@ -1,8 +1,12 @@
+from typing import List
+
 import uvicorn as uvicorn
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException, status
 from starlette.middleware.cors import CORSMiddleware
 
-from Server.models import ArduinoResponse, ArduinoAction, ActionType
+from Server.models import ArduinoResponse, ArduinoAction, ActionType, ActionsPostBody
+
+API_KEY = "123" # input your API key here or read it from env
 
 app = FastAPI()
 app.add_middleware(
@@ -12,16 +16,55 @@ app.add_middleware(
     allow_headers=["*"],  # Adjust this to restrict headers if needed
 )
 
-@app.get("/test/",
+@app.get("/",
          response_model=ArduinoResponse)
-def test():
-    response = ArduinoResponse(
-        actions = [
-            ArduinoAction(type=ActionType.TEXT, value="To jest taki dluzszy tekst aaaaaaaaaaaaaaaaaa"),
-            ArduinoAction(type=ActionType.SPIN, value=""),
-        ]
+async def get_actions(api_key: str):
+    if api_key != API_KEY:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect API KEY"
+        )
+
+    read_data_list = []
+    current_dict = {}
+    with open("buffer_file.txt", "r") as file:
+        # Read lines from the file and convert them back to a list of dictionaries (deserialize)
+        for line in file:
+            if line.strip() == "=====":
+                # A separator indicates the end of a dictionary, so add the current_dict to the list
+                read_data_list.append(current_dict)
+                current_dict = {}
+            else:
+                key, value = line.strip().split(": ")
+                current_dict[key] = value
+
+    # clear buffer file
+    with open("buffer_file.txt", "w") as file:
+        pass
+
+    actions: List[ArduinoAction] = [ArduinoAction(type=ActionType[action["type"].split('.')[1]], value=action["value"]) for action in read_data_list]
+    return ArduinoResponse(
+        actions = actions
     )
-    return response
+
+
+@app.post("/")
+async def post_actions(api_key: str, actions: ActionsPostBody):
+    if api_key != API_KEY:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect API KEY"
+        )
+
+    with open("buffer_file.txt", "w") as file:
+        # Write the dictionary to the file as a string (serialize)
+        for action in actions.actions:
+            for key, value in action.model_dump().items():
+                file.write(f"{key}: {value}\n")
+            file.write("=====\n")
+
+    return {"message": "ok"}
+
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=80)
--- Server/models.py
+++ Server/models.py
@@ -10,6 +10,22 @@ class ActionType(Enum):
 class ArduinoAction(BaseModel):
     type: ActionType
     value: str = "" #max 31 characters
+
+class MessageAction(ArduinoAction):
+    type: ActionType = ActionType.TEXT
+    @classmethod
+    def create(cls, text: str):
+        if len(text) < 32:
+            return cls(value=text)
+
+class SpinAction(ArduinoAction):
+    type: ActionType = ActionType.SPIN
+    @classmethod
+    def create(cls):
+        return cls()
 class ArduinoResponse(BaseModel):
     actions: List[ArduinoAction]
+
+class ActionsPostBody(ArduinoResponse):
+    pass
 
